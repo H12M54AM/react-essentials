f11d99293a5433a7cc9a4681868876e307fcb1f9
Edward Naidoo <securedbread@protonmail.com>
2022-01-14 09:26:08 -0800

    Deleted many things

--- src/App.js
+++ src/App.js
@@ -1,65 +1,19 @@
 import './App.css';
-import logo from "./logo.svg"
 
-function Header(props) {
-  return (
-    <header>
-      <h1>
-        {props.names}'s Website
-      </h1>
-    </header>
-  );
+function secretComponent() {
+  return <h1>Secret stuff, you see, you die. BING BONG.</h1>;
 }
 
-function Main(props) {
-  return (
-    <section>
-      <img src={logo} height={200} alt='Using the React logo for store logofor fun'/>
-      <h3>
-        Welcome to The Atomic Slice
-      </h3>
-      <p>Menu</p>
-      {/* Lists all of the arrays with an id and title */}
-      <ul style={{textAlign: "left"}}>
-        {props.dishes.map((dish) => (
-          <li key={dishes.id}>{dish.title}</li>
-        ))}
-      </ul>
-      
-    </section>
-  );
+function yesComponent() {
+  return <h1>Not so secret things. You good.</h1>;
 }
 
-function Footer(props) {
-  return (
-    <footer>
-    <h3>
-      Today is {props.month}/{props.today}/{props.year}
-    </h3>
-    <p>
-      @Copyright {props.year}
-    </p>
-    </footer>
-  );
-}
-
-const dishes = [
-  "Roasted Steak",
-  "Chicken Stew",
-  "Veggie Burger"
-]
-
-// Transformation objects
-const dishObjects = dishes.map((dish, i) => ({id: i, title: dish }));
-
-function App() {
-  return (
-    <div className="App">
-      <Header names="Edward" />
-      <Main dishes={dishObjects}/>
-      <Footer year={new Date().getFullYear()} today={new Date().getDate()} month={new Date().getMonth()}/>
-    </div>
-  );
+function App(props) {
+  if (props.authorized) {
+    <secretComponent />
+  } else {
+    <yesComponent />
+  }
 }
 
 export default App;
