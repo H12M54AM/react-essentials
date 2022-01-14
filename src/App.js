ac59d92d2ea2df22c9dd5fb7ff4c833e4722cff4
Edward Naidoo <securedbread@protonmail.com>
2022-01-14 08:58:55 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,63 @@
 import './App.css';
+import logo from "./logo.svg"
 
 function Header(props) {
   return (
     <header>
       <h1>
-        {props.names} Edward Naidoo
+        {props.names}'s Website
       </h1>
     </header>
   );
 }
 
-function Main() {
+function Main(props) {
   return (
     <section>
-      <p>
-        This is my first react website
-      </p>
+      <img src={logo} height={200} alt='Using the React logo for store logofor fun'/>
+      <h3>
+        Welcome to The Atomic Slice
+      </h3>
+      <p>Menu</p>
+      {/* Lists all of the arrays with an id and title */}
+      <ul style={{textAlign: "left"}}>
+        {props.dishes.map((dish) => (
+          <li key={dishes.id}>{dish.title}</li>
+        ))}
+      </ul>
+      
     </section>
   );
 }
 
+function Footer(props) {
+  return (
+    <footer>
+    <h3>
+      Today is {props.month}/{props.today}/{props.year}
+    </h3>
+    <p>
+      @Copyright {props.year}
+    </p>
+    </footer>
+  );
+}
+
+const dishes = [
+  "Roasted Steak",
+  "Chicken Stew",
+  "Veggie Burger"
+]
+
+// Transformation objects
+const dishObjects = dishes.map((dish, i) => ({id: i, title: dish }));
+
 function App() {
   return (
     <div className="App">
-      <Header names="Bing Bong" />
-      <Main />
+      <Header names="Edward" />
+      <Main dishes={dishObjects}/>
+      <Footer year={new Date().getFullYear()} today={new Date().getDate()} month={new Date().getMonth()}/>
     </div>
   );
 }
