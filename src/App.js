2621e7e0002a5bcd9ac85332620be80eccfd0f97
Edward Naidoo <securedbread@protonmail.com>
2022-01-15 17:44:48 -0800

    Removed Existing Code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import './App.css';
 
-function secretComponent() {
-  return <h1>Secret stuff, you see, you die. BING BONG.</h1>;
-}
+
 function normalComponent() {
-  return <p>Hello </p>
+  <p>Hello </p>
 }
 
 function yesComponent() {
-  return <h1>Not so secret things. You good.</h1>;
+  <h1>Not so secret things. You good.</h1>;
 }
 
-function App(props) {
-  <normalComponent />
-  if (props.authorized) {
-    return <secretComponent />
-  } else {
-    return <yesComponent />
-  }
+function App() {
+  return (
+    <normalComponent />,
+    <secretComponent />,
+    <yesComponent />
+  );
 }
 
 export default App;
