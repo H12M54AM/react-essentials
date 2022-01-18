48059dca0ef03369436f058748fb3b86377144ca
Edward Naidoo <securedbread@protonmail.com>
2022-01-17 23:50:40 -0800

    Update App.js
    Added the useState and useEffect into the code and made buttons that send a log into the browser, not the window itself. It allows people to press said emotion avaliable and record that state in the log

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState}from 'react';
+import React, {useState, useEffect}from 'react';
 import './App.css';
 
 
@@ -12,16 +12,48 @@ function Header() {
   );
 }
 
-function App() {
-  const [emotion, setEmotion] = useState('Happy');
-
+function Emotion() {
+    const [emotion, setEmotion] = useState('Happy');
+    const [secemotion, setSecEmotion] = useState('Tired');
+    
+    useEffect(() => {
+      console.log(`It is ${emotion} around here!`)
+    }, [emotion]);
+    useEffect(() => {
+      console.log(`It is ${secemotion} around here!`)
+    }, [secemotion]);
+    
   return (
     <>
       <h1>
-        I currently am {emotion}
+        I am currently {emotion} and {secemotion}
       </h1>
       <button onClick={() => setEmotion("Angry")}>Angry</button>
-      <Header />,
+      <button onClick={() => setEmotion("Sad")}>Sad</button>
+      <button onClick={() => setEmotion("Confused")}>Confused</button>
+    
+      <button onClick={() => setSecEmotion("Angsty")}>Angsty</button>
+      <button onClick={() => setSecEmotion("Wierd")}>Wierd</button>
+      <button onClick={() => setSecEmotion("Furious")}>Furious</button>
+    </>
+  );
+}
+
+function CheckBox() {
+  return(
+    <>
+      <input type="checkbox"/>
+    </>
+  );
+}
+
+function App() {
+
+  return (
+    <>
+      <Emotion />
+      <Header />
+      <CheckBox />
     </>
   );
 }
